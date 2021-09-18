632211fc49f301733a00cbebb043dc543565ceb9
Jonathan Martin <oldschoolspaceempire@gmail.com>
2021-09-18 08:48:21 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+//set up view engine
+app.set('view engine', 'ejs');
+
+// create home route
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.listen(5000, () => {
+	console.log('app now listening on port 5000');
+});
